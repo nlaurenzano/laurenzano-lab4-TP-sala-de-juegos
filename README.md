94d2d05087b4e7ebec58e788307b83031f4100ae
Nicolas Laurenzano <nlaurenzano@gmail.com>
2023-09-09 22:11:34 -0300

    Sprint 1: Componentes y routing. Falta diseño y contenido.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # laurenzano-lab4-TP-sala-de-juegos
+
+Sprint 1: Entrega en clase 03 - 14/9
+
+- Armado del proyecto
+- Subido a hosting WEB.
+- Componente de Login.
+- Componente de Home.
+- Componente “Quién Soy”
+- Favicon
+
+
+
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { WhoamiComponent } from './components/whoami/whoami.component';
+
+const routes: Routes = [
+  {
+    path:"", redirectTo:'/home', pathMatch:'full'
+  },
+  {
+    path:"home",
+    component: HomeComponent
+  },
+  {
+    path:"login",
+    component: LoginComponent
+  },
+  {
+    path:"whoami",
+    component: WhoamiComponent
+  },
+  {
+    path:"**",
+    component: HomeComponent
+    // component: NotFoundComponent
+  }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
